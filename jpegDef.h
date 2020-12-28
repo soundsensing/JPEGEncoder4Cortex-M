4361b8faab3c149f065b5b08877a99937fab5bda
Jon Nordby <jononor@gmail.com>
2020-12-28 19:55:09 +0100

    Use stdint.h instead of custom defines for fixed-size integers

--- jpegDef.h
+++ jpegDef.h
@@ -22,16 +22,9 @@
  */
 /* #define __FPU_USED 1 */
 
-typedef          char       int8_t;
-typedef          int        int32_t;
-typedef unsigned int        uint32_t;
+#include <stdint.h>
 
-typedef          short      int16_t;
 typedef          int        int_t;
-typedef unsigned char       uint8_t;
-typedef unsigned short      uint16_t;
-typedef unsigned int        uint_t;
-typedef unsigned char       uint8_t;
 typedef unsigned int        uint_t;
 
 #endif
